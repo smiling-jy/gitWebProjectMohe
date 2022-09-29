1e082a72872dd1c8391a83eaa6e42efd67fb1fe5
boram <108602668+robam1108@users.noreply.github.com>
2022-09-29 16:40:08 +0900

    VO.java update

---
+++ src/main/java/com/project/mohe/domain/Funding_qnaVO.java
@@ -0,0 +1,50 @@
+package com.project.mohe.domain;
+
+public class Funding_qnaVO {
+
+	private int qna_fd_no;
+	private int qna_no;
+	private int qna_user_no;
+	private String qna_question;
+	private String qna_answer;
+	
+	
+	public int getQna_fd_no() {
+		return qna_fd_no;
+	}
+	public void setQna_fd_no(int qna_fd_no) {
+		this.qna_fd_no = qna_fd_no;
+	}
+	public int getQna_no() {
+		return qna_no;
+	}
+	public void setQna_no(int qna_no) {
+		this.qna_no = qna_no;
+	}
+	public int getQna_user_no() {
+		return qna_user_no;
+	}
+	public void setQna_user_no(int qna_user_no) {
+		this.qna_user_no = qna_user_no;
+	}
+	public String getQna_question() {
+		return qna_question;
+	}
+	public void setQna_question(String qna_question) {
+		this.qna_question = qna_question;
+	}
+	public String getQna_answer() {
+		return qna_answer;
+	}
+	public void setQna_answer(String qna_answer) {
+		this.qna_answer = qna_answer;
+	}
+	
+	
+
+
+
+
+
+
+}
