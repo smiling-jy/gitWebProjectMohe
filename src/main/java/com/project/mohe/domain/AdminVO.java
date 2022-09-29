e319ecbb30cc21485ca9d2c05345f268fcfa07f2
human <human@DESKTOP-VGBLM31>
2022-09-29 18:26:02 +0900

    adminvo update

---
+++ src/main/java/com/project/mohe/domain/AdminVO.java
@@ -0,0 +1,70 @@
+package com.project.mohe.domain;
+
+public class AdminVO {
+	
+	private int adm_no;
+	private String adm_id;
+	private String adm_pass;
+	private String adm_name;
+	private String adm_dept;
+	private String adm_phone;
+	private String adm_adress;
+	private String adm_hiredate;
+	private String adm_enddate;
+	
+	public int getAdm_no() {
+		return adm_no;
+	}
+	public void setAdm_no(int adm_no) {
+		this.adm_no = adm_no;
+	}
+	public String getAdm_id() {
+		return adm_id;
+	}
+	public void setAdm_id(String adm_id) {
+		this.adm_id = adm_id;
+	}
+	public String getAdm_pass() {
+		return adm_pass;
+	}
+	public void setAdm_pass(String adm_pass) {
+		this.adm_pass = adm_pass;
+	}
+	public String getAdm_name() {
+		return adm_name;
+	}
+	public void setAdm_name(String adm_name) {
+		this.adm_name = adm_name;
+	}
+	public String getAdm_dept() {
+		return adm_dept;
+	}
+	public void setAdm_dept(String adm_dept) {
+		this.adm_dept = adm_dept;
+	}
+	public String getAdm_phone() {
+		return adm_phone;
+	}
+	public void setAdm_phone(String adm_phone) {
+		this.adm_phone = adm_phone;
+	}
+	public String getAdm_adress() {
+		return adm_adress;
+	}
+	public void setAdm_adress(String adm_adress) {
+		this.adm_adress = adm_adress;
+	}
+	public String getAdm_hiredate() {
+		return adm_hiredate;
+	}
+	public void setAdm_hiredate(String adm_hiredate) {
+		this.adm_hiredate = adm_hiredate;
+	}
+	public String getAdm_enddate() {
+		return adm_enddate;
+	}
+	public void setAdm_enddate(String adm_enddate) {
+		this.adm_enddate = adm_enddate;
+	}
+
+}
