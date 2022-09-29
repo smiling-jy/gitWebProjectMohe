830338c03d054aa65206350071eef29d9587b4d4
human <human@DESKTOP-VGBLM31>
2022-09-29 18:45:25 +0900

    영민언니 대신 update

---
+++ src/main/java/com/project/mohe/dao/noticeDAOImpl.java
@@ -0,0 +1,49 @@
+package com.project.mohe.dao;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.project.mohe.domain.BoardVO;
+
+@Repository("NoticeDAO")
+public class noticeDAOImpl implements noticeDAO {
+
+	@Override
+	public void insertNotice(BoardVO vo) {
+		// TODO Auto-generated method stub
+		
+	} 
+
+	@Override
+	public void updateNotice(BoardVO vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteNotice(BoardVO vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void incReadCount(BoardVO vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public BoardVO getNotice(BoardVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<BoardVO> getNoticeList(HashMap map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
