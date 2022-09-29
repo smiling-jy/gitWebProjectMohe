10c2c18f4ddb458f3ba61a775945f13ddacdd073
human <human@DESKTOP-VGBLM31>
2022-09-29 19:17:44 +0900

    2022.09.29 last update

---
+++ src/main/java/com/project/mohe/domain/DonationVO.java
@@ -0,0 +1,47 @@
+package com.project.mohe.domain;
+
+public class DonationVO {
+	private int dnt_no;
+	private String dnt_name;
+	private String dnt_phone;
+	private String dnt_amount;
+	private String dnt_receipt;
+	private String dnt_indate;
+	
+	public int getDnt_no() {
+		return dnt_no;
+	}
+	public void setDnt_no(int dnt_no) {
+		this.dnt_no = dnt_no;
+	}
+	public String getDnt_name() {
+		return dnt_name;
+	}
+	public void setDnt_name(String dnt_name) {
+		this.dnt_name = dnt_name;
+	}
+	public String getDnt_phone() {
+		return dnt_phone;
+	}
+	public void setDnt_phone(String dnt_phone) {
+		this.dnt_phone = dnt_phone;
+	}
+	public String getDnt_amount() {
+		return dnt_amount;
+	}
+	public void setDnt_amount(String dnt_amount) {
+		this.dnt_amount = dnt_amount;
+	}
+	public String getDnt_receipt() {
+		return dnt_receipt;
+	}
+	public void setDnt_receipt(String dnt_receipt) {
+		this.dnt_receipt = dnt_receipt;
+	}
+	public String getDnt_indate() {
+		return dnt_indate;
+	}
+	public void setDnt_indate(String dnt_indate) {
+		this.dnt_indate = dnt_indate;
+	}
+}
---
+++ src/main/java/com/project/mohe/domain/Volunteer_cartVO.java
@@ -0,0 +1,35 @@
+package com.project.mohe.domain;
+
+public class Volunteer_cartVO {
+	private int fc_no;
+	private int fd_no;
+	private int user_no;
+	private String vc_date_time;
+	
+	public int getFc_no() {
+		return fc_no;
+	}
+	public void setFc_no(int fc_no) {
+		this.fc_no = fc_no;
+	}
+	public int getFd_no() {
+		return fd_no;
+	}
+	public void setFd_no(int fd_no) {
+		this.fd_no = fd_no;
+	}
+	public int getUser_no() {
+		return user_no;
+	}
+	public void setUser_no(int user_no) {
+		this.user_no = user_no;
+	}
+	public String getVc_date_time() {
+		return vc_date_time;
+	}
+	public void setVc_date_time(String fc_date_time) {
+		this.vc_date_time = fc_date_time;
+	}
+
+	
+}
