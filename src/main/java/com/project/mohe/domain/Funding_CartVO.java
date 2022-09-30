4fa0f4895a27a609ad43f73c972f03c60dd0ce82
human <human@DESKTOP-VGBLM31>
2022-09-30 13:59:01 +0900

    09.30 롬북 업데이트 -지윤

--- src/main/java/com/project/mohe/domain/Funding_CartVO.java
+++ src/main/java/com/project/mohe/domain/Funding_CartVO.java
@@ -1,35 +1,11 @@
 package com.project.mohe.domain;
+import lombok.Data;
 
+@Data
 public class Funding_CartVO {
 	private int fc_no;
 	private int fd_no;
 	private int user_no;
 	private String fc_date_time;
-	
-	public int getFc_no() {
-		return fc_no;
-	}
-	public void setFc_no(int fc_no) {
-		this.fc_no = fc_no;
-	}
-	public int getFd_no() {
-		return fd_no;
-	}
-	public void setFd_no(int fd_no) {
-		this.fd_no = fd_no;
-	}
-	public int getUser_no() {
-		return user_no;
-	}
-	public void setUser_no(int user_no) {
-		this.user_no = user_no;
-	}
-	public String getFc_date_time() {
-		return fc_date_time;
-	}
-	public void setFc_date_time(String fc_date_time) {
-		this.fc_date_time = fc_date_time;
-	}
-
 	
 }
--- src/main/java/com/project/mohe/domain/eventVO.java
+++ src/main/java/com/project/mohe/domain/eventVO.java
@@ -1,5 +1,7 @@
 package com.project.mohe.domain;
+import lombok.Data;
 
+@Data
 public class eventVO {
 	
 	private int event_no;
@@ -7,39 +9,5 @@ public class eventVO {
 	private String event_title;
 	private String event_text;
 	private String event_img;
-	
-	 
-	
-	public int getEvent_no() {
-		return event_no;
-	}
-	public void setEvent_no(int event_no) {
-		this.event_no = event_no;
-	}
-	public int getAdm_no() {
-		return adm_no;
-	}
-	public void setAdm_no(int adm_no) {
-		this.adm_no = adm_no;
-	}
-	public String getEvent_title() {
-		return event_title;
-	}
-	public void setEvent_title(String event_title) {
-		this.event_title = event_title;
-	}
-	public String getEvent_text() {
-		return event_text;
-	}
-	public void setEvent_text(String event_text) {
-		this.event_text = event_text;
-	}
-	public String getEvent_img() {
-		return event_img;
-	}
-	public void setEvent_img(String event_img) {
-		this.event_img = event_img;
-	}
-	
 	
 }
