b3dd9ff24dbb04eb42f308a32873c35b79349e71
human <human@DESKTOP-VGBLM31>
2022-10-04 09:51:02 +0900

    10.04 dao&service 경로 수정 완료 -지윤

---
+++ src/main/java/com/project/mohe/service/impl/DonationServiceImpl.java
@@ -0,0 +1,50 @@
+package com.project.mohe.service.impl;
+
+import java.util.HashMap;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.mohe.dao.DonationDAO;
+import com.project.mohe.domain.AdminVO;
+import com.project.mohe.domain.DonationVO;
+import com.project.mohe.service.DonationService;
+
+@Service("donationService")
+public class DonationServiceImpl implements DonationService {
+
+	@Autowired
+	private DonationDAO donationDao;
+
+	@Override
+	public void insertDonation(DonationVO vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateDonation(DonationVO vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteDonation(DonationVO vo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public DonationVO getDonation(DonationVO vo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<DonationVO> getDonationList(HashMap map) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
